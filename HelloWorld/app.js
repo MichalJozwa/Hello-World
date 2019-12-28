b3fff745c18ff7b9ad7e1592d38fd94615c559e4
Michal Jozwa <57333359+MichalJozwa@users.noreply.github.com>
2019-12-28 03:51:26 +0100

    Update app.js

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,33 +1,3 @@
-/* TO DO list:
- - preloader
- - language option
- - carousel
-*/
-
-// if html has attribute lang="en", language on site is english and the span of div.language has english text
-
-// var polish = document.querySelector(".pl"),
-//     english = document.querySelector(".en"),
-//     button = document.querySelector('.lang-btn');
-
-// var button = document.querySelector('.btn');
-// var p = document.querySelectorAll('.about-paragraph');
-
-// button.addEventListener("click", function(){
-//     if(this.checked){
-//         this.setAttribute("checked", "true")
-//         document.documentElement.setAttribute('lang', 'pl')
-//     } else{
-//         this.setAttribute("checked", "false")
-//         document.documentElement.setAttribute('lang', 'en')
-//     }
-// })
-
-// Language
-// var button = document.querySelector('lang-btn');
-
-// button.addEventListener('click', )
-
 // Dark Theme
 var checkbox = document.querySelector('input[name=theme]');
 
