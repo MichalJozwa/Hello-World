4e6756cfaae7c25d7999acaf3a1aa248ddf35d40
Michal Jozwa <57333359+MichalJozwa@users.noreply.github.com>
2019-12-28 10:39:13 +0100

    Add files via upload

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,3 +1,33 @@
+/* TO DO list:
+ - preloader
+ - language option
+ - carousel
+*/
+
+// if html has attribute lang="en", language on site is english and the span of div.language has english text
+
+// var polish = document.querySelector(".pl"),
+//     english = document.querySelector(".en"),
+//     button = document.querySelector('.lang-btn');
+
+// var button = document.querySelector('.btn');
+// var p = document.querySelectorAll('.about-paragraph');
+
+// button.addEventListener("click", function(){
+//     if(this.checked){
+//         this.setAttribute("checked", "true")
+//         document.documentElement.setAttribute('lang', 'pl')
+//     } else{
+//         this.setAttribute("checked", "false")
+//         document.documentElement.setAttribute('lang', 'en')
+//     }
+// })
+
+// Language
+// var button = document.querySelector('lang-btn');
+
+// button.addEventListener('click', )
+
 // Dark Theme
 var checkbox = document.querySelector('input[name=theme]');
 
@@ -15,5 +45,5 @@ let trans = () => {
     document.documentElement.classList.add('transition');
     window.setTimeout(() => {
         document.documentElement.classList.remove('transition');
-    }, 1000)
+    }, 800)
 }
